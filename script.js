19caeadc4c57b317bce22ace982ca0169e1592fb
Alex Gibson <argibson02@gmail.com>
2021-09-29 23:41:31 -0700

    Local storage now works on all fields.

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 var timeNow = moment();
 var timeNowUser = moment().format("dddd, DD-MM-YYYY, kk:mm:ss");
 var notesArray = ["","","","","","","","","","","","","","","","","","","","","","","",""];
-var localCheck = ["","","","","","","","","","","","","","","","","","","","","","","",""];
+var localCheck = [];
+var localCheckVerify = ["","","","","","","","","","","","","","","","","","","","","","","",""];
 var notesInput = document.getElementById("initialsText");
 
 //textarea variables
@@ -74,9 +75,9 @@ setInterval(currentTimeClock, 1000);
 
 
 //Needs:
-//- clock running with moment.js and updating every second
-//- event listeners on all buttons
-//- notes and send to local storage
+//- clock running with moment.js and updating every second   ---DONE
+//- event listeners on all buttons --- DONE, but could be jQuery ones
+//- notes and send to local storage ---DONE
 //- retrieve from local storage
 //- change text area background colors dynamically by hour
 //
@@ -88,24 +89,53 @@ function colorSwap() {
 }
 colorSwap();
 */
+
+
 //----------------------------STORAGE
 //----------------------------------------------------Function for any stored notes
-//function checkNotes() {
-//    if (localStorage.getItem("notesArray") === null) { // if the local storage array is null, we skip syncing
-//        return;
-//    }
-//    else if (localStorage.getItem("notesArray") === "") { // if the local storage array is an empty string, we skip syncing
-//        return;
-//    }
-//    else {
-//        localCheck = JSON.parse(localStorage.getItem("notesArray")); // if not null, make it var local check
-//    }
-//
-//    if (localCheck.length > notesArray.length) { // if local storage is longer, we make it into the function array
-//        notesArray = localCheck;
-//    }
-//}
-//checkNotes();
+function checkNotes() {
+    if (localStorage.getItem("notesArray") === null) { // if the local storage array is null, we skip syncing
+        return;
+    }
+    else if (localStorage.getItem("notesArray") === "") { // if the local storage array is an empty string, we skip syncing
+        return;
+    }
+    else {
+        localCheck = JSON.parse(localStorage.getItem("notesArray")); // if not null, make it var local check
+        //console.log(notesArray + "1");
+    }
+
+    if (localCheck !== localCheckVerify) { // if local storage is longer, we make it into the function array
+        //console.log(notesArray + "2");
+        notesArray = localCheck;
+        //console.log(notesArray + "3");
+        text00.value = notesArray[00];
+        text01.value = notesArray[01];
+        text02.value = notesArray[02];
+        text03.value = notesArray[03];
+        text04.value = notesArray[04];
+        text05.value = notesArray[05];
+        text06.value = notesArray[06];
+        text07.value = notesArray[07];
+        text08.value = notesArray[08];
+        text09.value = notesArray[09];
+        text10.value = notesArray[10];
+        text11.value = notesArray[11];
+        text12.value = notesArray[12];
+        text13.value = notesArray[13];
+        text14.value = notesArray[14];
+        text15.value = notesArray[15];
+        text16.value = notesArray[16];
+        text17.value = notesArray[17];
+        text18.value = notesArray[18];
+        text19.value = notesArray[19];
+        text20.value = notesArray[20];
+        text21.value = notesArray[21];
+        text22.value = notesArray[22];
+        text23.value = notesArray[23];
+    }
+}
+checkNotes();
 
 
 
@@ -171,7 +201,6 @@ function storeNotes03(event) {
         notesArray = JSON.parse(localStorage.getItem("notesArray")); //Array is stored as string in local storage. Grabbing it as an array and re-syncing the javascript array with local;
 }
 
-
 function storeNotes04(event) {
     event.preventDefault();
     if (text04.value === "") {
@@ -200,7 +229,6 @@ function storeNotes05(event) {
         notesArray = JSON.parse(localStorage.getItem("notesArray")); //Array is stored as string in local storage. Grabbing it as an array and re-syncing the javascript array with local;
 }
 
-
 function storeNotes06(event) {
     event.preventDefault();
     if (text06.value === "") {
@@ -214,7 +242,6 @@ function storeNotes06(event) {
         }
         notesArray = JSON.parse(localStorage.getItem("notesArray")); //Array is stored as string in local storage. Grabbing it as an array and re-syncing the javascript array with local;
 }
-
 
 function storeNotes07(event) {
     event.preventDefault();
@@ -230,7 +257,6 @@ function storeNotes07(event) {
         notesArray = JSON.parse(localStorage.getItem("notesArray")); //Array is stored as string in local storage. Grabbing it as an array and re-syncing the javascript array with local;
 }
 
-
 function storeNotes08(event) {
     event.preventDefault();
     if (text08.value === "") {
@@ -244,7 +270,6 @@ function storeNotes08(event) {
         }
         notesArray = JSON.parse(localStorage.getItem("notesArray")); //Array is stored as string in local storage. Grabbing it as an array and re-syncing the javascript array with local;
 }
-
 
 function storeNotes09(event) {
     event.preventDefault();
@@ -260,7 +285,6 @@ function storeNotes09(event) {
         notesArray = JSON.parse(localStorage.getItem("notesArray")); //Array is stored as string in local storage. Grabbing it as an array and re-syncing the javascript array with local;
 }
 
-
 function storeNotes10(event) {
     event.preventDefault();
     if (text10.value === "") {
@@ -274,7 +298,6 @@ function storeNotes10(event) {
         }
         notesArray = JSON.parse(localStorage.getItem("notesArray")); //Array is stored as string in local storage. Grabbing it as an array and re-syncing the javascript array with local;
 }
-
 
 function storeNotes11(event) {
     event.preventDefault();
@@ -290,7 +313,6 @@ function storeNotes11(event) {
         notesArray = JSON.parse(localStorage.getItem("notesArray")); //Array is stored as string in local storage. Grabbing it as an array and re-syncing the javascript array with local;
 }
 
-
 function storeNotes12(event) {
     event.preventDefault();
     if (text12.value === "") {
@@ -304,7 +326,6 @@ function storeNotes12(event) {
         }
         notesArray = JSON.parse(localStorage.getItem("notesArray")); //Array is stored as string in local storage. Grabbing it as an array and re-syncing the javascript array with local;
 }
-
 
 function storeNotes13(event) {
     event.preventDefault();
@@ -320,7 +341,6 @@ function storeNotes13(event) {
         notesArray = JSON.parse(localStorage.getItem("notesArray")); //Array is stored as string in local storage. Grabbing it as an array and re-syncing the javascript array with local;
 }
 
-
 function storeNotes14(event) {
     event.preventDefault();
     if (text14.value === "") {
@@ -334,7 +354,6 @@ function storeNotes14(event) {
         }
         notesArray = JSON.parse(localStorage.getItem("notesArray")); //Array is stored as string in local storage. Grabbing it as an array and re-syncing the javascript array with local;
 }
-
 
 function storeNotes15(event) {
     event.preventDefault();
@@ -350,7 +369,6 @@ function storeNotes15(event) {
         notesArray = JSON.parse(localStorage.getItem("notesArray")); //Array is stored as string in local storage. Grabbing it as an array and re-syncing the javascript array with local;
 }
 
-
 function storeNotes16(event) {
     event.preventDefault();
     if (text16.value === "") {
@@ -364,7 +382,6 @@ function storeNotes16(event) {
         }
         notesArray = JSON.parse(localStorage.getItem("notesArray")); //Array is stored as string in local storage. Grabbing it as an array and re-syncing the javascript array with local;
 }
-
 
 function storeNotes17(event) {
     event.preventDefault();
@@ -380,7 +397,6 @@ function storeNotes17(event) {
         notesArray = JSON.parse(localStorage.getItem("notesArray")); //Array is stored as string in local storage. Grabbing it as an array and re-syncing the javascript array with local;
 }
 
-
 function storeNotes18(event) {
     event.preventDefault();
     if (text18.value === "") {
@@ -394,7 +410,6 @@ function storeNotes18(event) {
         }
         notesArray = JSON.parse(localStorage.getItem("notesArray")); //Array is stored as string in local storage. Grabbing it as an array and re-syncing the javascript array with local;
 }
-
 
 function storeNotes19(event) {
     event.preventDefault();
@@ -410,7 +425,6 @@ function storeNotes19(event) {
         notesArray = JSON.parse(localStorage.getItem("notesArray")); //Array is stored as string in local storage. Grabbing it as an array and re-syncing the javascript array with local;
 }
 
-
 function storeNotes20(event) {
     event.preventDefault();
     if (text20.value === "") {
@@ -424,7 +438,6 @@ function storeNotes20(event) {
         }
         notesArray = JSON.parse(localStorage.getItem("notesArray")); //Array is stored as string in local storage. Grabbing it as an array and re-syncing the javascript array with local;
 }
-
 
 function storeNotes21(event) {
     event.preventDefault();
@@ -440,7 +453,6 @@ function storeNotes21(event) {
         notesArray = JSON.parse(localStorage.getItem("notesArray")); //Array is stored as string in local storage. Grabbing it as an array and re-syncing the javascript array with local;
 }
 
-
 function storeNotes22(event) {
     event.preventDefault();
     if (text22.value === "") {
@@ -454,7 +466,6 @@ function storeNotes22(event) {
         }
         notesArray = JSON.parse(localStorage.getItem("notesArray")); //Array is stored as string in local storage. Grabbing it as an array and re-syncing the javascript array with local;
 }
-
 
 function storeNotes23(event) {
     event.preventDefault();
@@ -498,121 +509,113 @@ btn21.addEventListener("click", storeNotes21);
 btn22.addEventListener("click", storeNotes22);
 btn23.addEventListener("click", storeNotes23);
 
-/*
-00
-01
-02
-03
-04
-05
-06
-07
-08
-09
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-*/
 
-$('btn00').click(storeNotes00);
-$('btn01').click(storeNotes01);
-$('btn02').click(storeNotes02);
-$('btn03').click(storeNotes03);
-$('btn04').click(storeNotes04);
-$('btn05').click(storeNotes05);
-$('btn06').click(storeNotes06);
-$('btn07').click(storeNotes07);
-$('btn08').click(storeNotes08);
-$('btn09').click(storeNotes09);
-$('btn10').click(storeNotes10);
-$('btn11').click(storeNotes11);
-$('btn12').click(storeNotes12);
-$('btn13').click(storeNotes13);
-$('btn14').click(storeNotes14);
-$('btn15').click(storeNotes15);
-$('btn16').click(storeNotes16);
-$('btn17').click(storeNotes17);
-$('btn18').click(storeNotes18);
-$('btn19').click(storeNotes19);
-$('btn20').click(storeNotes20);
-$('btn21').click(storeNotes21);
-$('btn22').click(storeNotes22);
-$('btn23').click(storeNotes23);
+// jquesy event listners that dont work
+//$('btn00').click(storeNotes00);
+//$('btn01').click(storeNotes01);
+//$('btn02').click(storeNotes02);
+//$('btn03').click(storeNotes03);
+//$('btn04').click(storeNotes04);
+//$('btn05').click(storeNotes05);
+//$('btn06').click(storeNotes06);
+//$('btn07').click(storeNotes07);
+//$('btn08').click(storeNotes08);
+//$('btn09').click(storeNotes09);
+//$('btn10').click(storeNotes10);
+//$('btn11').click(storeNotes11);
+//$('btn12').click(storeNotes12);
+//$('btn13').click(storeNotes13);
+//$('btn14').click(storeNotes14);
+//$('btn15').click(storeNotes15);
+//$('btn16').click(storeNotes16);
+//$('btn17').click(storeNotes17);
+//$('btn18').click(storeNotes18);
+//$('btn19').click(storeNotes19);
+//$('btn20').click(storeNotes20);
+//$('btn21').click(storeNotes21);
+//$('btn22').click(storeNotes22);
+//$('btn23').click(storeNotes23);
+//
 
 
-
-
-
-
-
-
-/*
-//----------------------------STORAGE
-//----------------------------------------------------Function for any stored notes
-function checkNotes() {
-    if (localStorage.getItem("notesArray") === null) { // if the local storage array is null, we skip syncing
-        return;
-    }
-    else if (localStorage.getItem("notesArray") === "") { // if the local storage array is an empty string, we skip syncing
-        return;
-    }
-    else {
-        localCheck = JSON.parse(localStorage.getItem("notesArray")); // if not null, make it var local check
-    }
-
-    if (localCheck.length > notesArray.length) { // if local storage is longer, we make it into the function array
-        notesArray = localCheck;
-    }
-}
-
-
-/*
-//-----------------------------------------------Saving and clearing initials and scores
-// Function to save scores
-function saveNotes(event) {
-    event.preventDefault();
-    if (initialsInput.value === "") {
-        return; // prevents null additions to the scoresArray
-    } 
-    else {
-    var initialsHandOff = initialsInput.value.toUpperCase();  // Combining score and initials into an single string
-    localStorage.setItem("initials", initialsHandOff);
-    var scoreHandOff = timeScore;
-    var scoreAndInitials = (initialsHandOff + ": " + scoreHandOff + " points");
-    
-    scoresArray.push(scoreAndInitials); // push that item to out javascript array
-    localStorage.setItem("scoresArray", JSON.stringify(scoresArray));  // syncing javascript array and local storage, add to local storage
-
-    // Clearing field and disabling further input
-    initialsInput.value = "";
-    document.getElementById("initialsText").disabled = true;
-    document.getElementById("submitButton").disabled = true;
-    }
-    scoresArray = JSON.parse(localStorage.getItem("scoresArray")); //Array is stored as string in local storage. Grabbing it as an array and re-syncing the javascript array with local
-    displayScores();
-}
-
-// Function for clearing scoresArray
-function clearScores (event) {
-    event.preventDefault();
-    localStorage.setItem("scoresArray", ""); // clears local storage
-    scoresArray = []; // clears javascript storage
-    scoreboardUl.innerHTML = ""; 
-}
-
-
-
-//--------------------------------------------------Event Listeners
-submitButton.$(document).on(("click", saveNotes);
-*/
+//00
+//01
+//02
+//03
+//04
+//05
+//06
+//07
+//08
+//09
+//10
+//11
+//12
+//13
+//14
+//15
+//16
+//17
+//18
+//19
+//20
+//21
+//22
+//23
+//
+//
+//
+//
+////----------------------------STORAGE
+////----------------------------------------------------Function for any stored notes
+//function checkNotes() {
+//    if (localStorage.getItem("notesArray") === null) { // if the local storage array is null, we skip syncing
+//        return;
+//    }
+//    else if (localStorage.getItem("notesArray") === "") { // if the local storage array is an empty string, we skip syncing
+//        return;
+//    }
+//    else {
+//        localCheck = JSON.parse(localStorage.getItem("notesArray")); // if not null, make it var local check
+//    }
+//
+//    if (localCheck.length > notesArray.length) { // if local storage is longer, we make it into the function array
+//        notesArray = localCheck;
+//    }
+//}
+//
+//
+//
+////-----------------------------------------------Saving and clearing initials and scores
+//// Function to save scores
+//function saveNotes(event) {
+//    event.preventDefault();
+//    if (initialsInput.value === "") {
+//        return; // prevents null additions to the scoresArray
+//    } 
+//    else {
+//    var initialsHandOff = initialsInput.value.toUpperCase();  // Combining score and initials into an single string
+//    localStorage.setItem("initials", initialsHandOff);
+//    var scoreHandOff = timeScore;
+//    var scoreAndInitials = (initialsHandOff + ": " + scoreHandOff + " points");
+//    
+//    scoresArray.push(scoreAndInitials); // push that item to out javascript array
+//    localStorage.setItem("scoresArray", JSON.stringify(scoresArray));  // syncing javascript array and local storage, add to local storage
+//
+//    // Clearing field and disabling further input
+//    initialsInput.value = "";
+//    document.getElementById("initialsText").disabled = true;
+//    document.getElementById("submitButton").disabled = true;
+//    }
+//    scoresArray = JSON.parse(localStorage.getItem("scoresArray")); //Array is stored as string in local storage. Grabbing it as an array and re-syncing the javascript array with local
+//    displayScores();
+//}
+//
+//// Function for clearing scoresArray
+//function clearScores (event) {
+//    event.preventDefault();
+//    localStorage.setItem("scoresArray", ""); // clears local storage
+//    scoresArray = []; // clears javascript storage
+//    scoreboardUl.innerHTML = ""; 
+//}
+//
